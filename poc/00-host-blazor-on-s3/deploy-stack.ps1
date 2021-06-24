1d30db31fa3d06520543af3ecd73c7c7674c9513
bobac <4517566+bo-bac@users.noreply.github.com>
2021-06-24 10:36:15 +0300

    add base POC 'blazor can be hosted on s3'

--- poc/00-host-blazor-on-s3/deploy-stack.ps1
+++ poc/00-host-blazor-on-s3/deploy-stack.ps1
@@ -1,22 +1,24 @@
-param ($profile='blambda-dev', $domain='blambda')
+param ($profile='blambda-dev', $domain='blambda', $buildConfig='Release')
 
 ### Create Stack
 aws --profile $profile `
 		cloudformation create-stack `
 			--stack-name "$domain-stack" `
-			--template-body file://./src/SetupS3Template/create-stack.template `
+			--template-body file://./src/Tyts.Aws.Templates/blambda-stack.template `
 			--parameters ParameterKey=RootDomainName,ParameterValue=$domain `
 						 #ParameterKey=Parm2,ParameterValue=test2 `
 
 
 ### Publish frontend
-$deploy = ".\_deploy\shell"
-$shell = ".\src\Tyts.Shall"
+$deploy = ".\_deploy\shall"
+$shall = ".\src\Tyts.Shall"
+
 
 # create folder to publish locally
 New-Item -Path $deploy -ItemType Directory -Force
 # rebuild and publish 
-dotnet publish $shell --output $deploy -c Release 
+dotnet publish $shall --output $deploy -c $buildConfig 
+
 
 # wait until bucket exist
 Write-Host "Waiting bucket is created..."
--- poc/00-host-blazor-on-s3/publish.ps1
+++ poc/00-host-blazor-on-s3/publish.ps1
@@ -1,11 +1,11 @@
 param ($profile='blambda-dev', $domain='blambda', $buildConfig='Release')
 
 # Publish frontend
-$deploy = ".\_deploy\shell"
-$shell = ".\src\Tyts.Shall"
+$deploy = ".\_deploy\shall"
+$shall = ".\src\Tyts.Shall"
 
 New-Item -Path $deploy -ItemType Directory -Force
-dotnet publish $shell --output $deploy -c $buildConfig 
+dotnet publish $shall --output $deploy -c $buildConfig
 
 # Upload frontend to S3
 Push-Location -Path "$deploy\wwwroot"
