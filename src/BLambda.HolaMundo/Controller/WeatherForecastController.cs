6a2e347b2da4ee9929e8df962006fa44dc818d99
bobac <4517566+bo-bac@users.noreply.github.com>
2021-07-13 11:26:53 +0300

    add API (HolaMundo) with latest hosted by AWS runtime 3.1
    will try to compare hot/cold start with the same API by custom runtime

---
+++ src/BLambda.HolaMundo/Controller/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLambda.HolaMundo.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Tyts.Abstractions.Data;
+
+namespace BLambda.HolaMundo.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //FackeRepository
+        private IRepositoryAsync<WeatherForecast> repository;
+
+        public WeatherForecastController(IRepositoryAsync<WeatherForecast> repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET api/weather-forecast
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            return await repository.GetAllAsync();
+        }
+
+        // GET api/weather-forecast/5
+        [HttpGet("{id}")]
+        public WeatherForecast Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // POST api/weather-forecast
+        [HttpPost]
+        public void Post([FromBody] WeatherForecast value)
+        {
+        }
+
+        // PUT api/weather-forecast/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] WeatherForecast value)
+        {
+        }
+
+        // DELETE api/weather-forecast/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/BLambda.HolaMundo/Data/StatupExtension.cs
@@ -0,0 +1,15 @@
+﻿using BLambda.HolaMundo.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Tyts.Abstractions.Data;
+
+namespace BLambda.HolaMundo.Data
+{
+    public static class StatupExtension
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services
+                    .AddScoped<IRepositoryAsync<WeatherForecast>, WeatherForecastRepository>();
+        }
+    }
+}
